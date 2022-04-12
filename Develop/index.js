2e38d94300d1d4feaf817ffdd01afe641c265b4b
Alex Lane <97alane@gmail.com>
2022-04-12 18:27:12 -0500

    added questions and created to write read me file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,80 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+.prompt(
+    {
+        type: 'prompt',
+        message: 'What is the name of the project?',
+        name: 'title',
+    },
+    {
+        type:'input',
+        message: 'What is the project about?please be as descriptive as possible.',
+        name: 'description',
+    },
+    {
+        type:'input',
+        message: 'Table of Contents',
+        name: 'table of contents',
+    },
+    {
+        type:'input',
+        message: 'What installation is needed to run this application?',
+        name: 'installation',
+    },
+    {
+        type:'input',
+        message: 'How is the app used?',
+        name: 'usage',
+    }, {
+        type:'input',
+        message: 'What license is used?',
+        name: 'license',
+    },
+    {
+        type:'input',
+        message: 'Who contributed to the project?',
+        name: 'contributors',
+    },
+    {
+        type:'input',
+        message: 'What is your Github username?',
+        name: 'username',
+    },
+    {
+        type:'input',
+        message: 'contact info?',
+        name: 'contact',
+    },
+   
+    
+
+)
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        console.log(fileName)
+        console.log(data)
+        if (err) {
+            return console.log(err)
+        } else{
+            console.log("You did it!")
+        }
+    })
+    
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
